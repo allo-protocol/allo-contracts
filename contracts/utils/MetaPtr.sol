b5faa5b9cb994c3240c192b27b03a77882ead1c4
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-03-15 07:09:04 +0100

    Fix github links

--- contracts/utils/MetaPtr.sol
+++ contracts/utils/MetaPtr.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.17;
 struct MetaPtr {
 
   /// @notice Protocol ID corresponding to a specific protocol.
-  /// More info at https://github.com/gitcoinco/grants-round/tree/main/packages/contracts/docs/MetaPtrProtocol.md
+  /// More info at https://github.com/allo-protocol/contracts/tree/main/docs/MetaPtrProtocol.md
   uint256 protocol;
   
   /// @notice Pointer to fetch metadata for the specified protocol
