a806c2e850ecd890962ccb322438ed108ce1175b
Josef Vacek <vacekj@outlook.com>
2023-03-27 12:46:25 +0200

    feat: make applications work

--- scripts/deployTest.ts
+++ scripts/deployTest.ts
@@ -1,17 +1,54 @@
 import hre, {ethers, upgrades} from "hardhat";
 import {encodeProgramParameters, encodeRoundParameters} from "./utils";
 import {AddressZero} from "@ethersproject/constants";
-// import chalk from "chalk";
+import {address} from "hardhat/internal/core/config/config-validation";
+
+const STATUS = {
+    PENDING: 0,
+    ACCEPTED: 1,
+    REJECTED: 2,
+    CANCELED: 3,
+};
+
+const buildNewState = (current: bigint, indexes: number[], statusArray: number[]) => {
+    let newState: bigint = current;
+
+    for (let i = 0; i < indexes.length; i++) {
+        const index = indexes[i];
+        const position = (index % 32) * 2;
+        const mask = BigInt(~(BigInt(3) << BigInt(position)));
+        newState =
+            (newState & mask) | (BigInt(statusArray[i]) << BigInt(position));
+    }
+
+    return newState.toString();
+};
 
 async function deployEverything() {
+    async function getCurrentBlock() {
+        // Get the current block
+        const blockN = await ethers.provider.getBlockNumber();
+        const block = await ethers.provider.getBlock(blockN);
+
+        // Extract the block number and timestamp
+        const blockNumber = block.number;
+        const timestamp = block.timestamp;
+
+        // Convert the timestamp to a human-readable UTC date
+        const utcDate = new Date(timestamp * 1000).toISOString();
+
+        // Log the block number, timestamp, and human-readable UTC date
+        console.log(`Block Number: ${blockNumber}`);
+        console.log(`Timestamp: ${timestamp}`);
+        console.log(`UTC Date: ${utcDate}`);
+    }
+
     const account = (await ethers.getSigners())[0];
     const networkName = hre.network.name;
 
-    // console.log(
-    //     `${chalk.bold("Deploy test round on ")} ${networkName} as ${
-    //         account.address
-    //     }`
-    // );
+    console.log(
+        `Deploy test round on ${networkName} as ${account.address}`
+    );
 
     console.log("Deploying ProjectRegistry...");
     const projectRegistryFactory = await ethers.getContractFactory(
@@ -101,9 +138,7 @@ async function deployEverything() {
     const merklePayoutStrategyImplContract =
         await merklePayoutStrategyImpl.deploy();
     await merklePayoutStrategyImplContract.initialize();
-    console.log(
-        `Deployed MerklePayoutStrategyImplementation to ${qfVotingImplContract.address}`
-    );
+    console.log(`Deployed MerklePayoutStrategyImplementation to ${qfVotingImplContract.address}`);
 
     let payoutLink =
         await merklePayoutStrategyFactoryContract.updatePayoutImplementation(
@@ -114,14 +149,24 @@ async function deployEverything() {
     /* Round */
     const roundFactory = await ethers.getContractFactory("RoundFactory");
     const roundFactoryContract = await upgrades.deployProxy(roundFactory);
+    console.log(`Deployed RoundFactory to ${roundFactoryContract.address}`);
+    const rfActualContract = await ethers.getContractAt("RoundFactory", roundFactoryContract.address);
+
+    const alloSettingsFactory = await ethers.getContractFactory("AlloSettings");
+    const alloSettingsContract = await alloSettingsFactory.deploy();
+    await alloSettingsContract.initialize();
+
+    await rfActualContract.updateAlloSettings(alloSettingsContract.address);
 
     const roundImpl = await ethers.getContractFactory("RoundImplementation");
     const roundImplContract = await roundImpl.deploy();
+    console.log(`Deployed RoundImplementation to ${roundImplContract.address}`);
 
     const linkRoundTx = await roundFactoryContract.updateRoundImplementation(
         roundImplContract.address
     );
     await linkRoundTx.wait();
+    console.log("Round link tx", linkRoundTx.hash);
 
     /* Create rojects */
     const registry = await ethers.getContractAt(
@@ -144,7 +189,13 @@ async function deployEverything() {
                 pointer: "bafybeih2pise44gkkzj7fdws3knwotppnh4x2gifnbxjtttuv7okw4mjzu",
             },
         },
-
+        {
+            project: "0x500Df079BEBE24A9f6FFa2c70fb58000A4722784",
+            metaPtr: {
+                protocol: 1,
+                pointer: "bafybeiceggy6uzfxsn3z6b2rraptp3g2kx2nrwailkjnx522yah43g5tyu",
+            },
+        },
         {
             project: "0x500Df079BEBE24A9f6FFa2c70fb58000A4722784",
             metaPtr: {
@@ -154,11 +205,10 @@ async function deployEverything() {
         },
     ];
 
-    await Promise.all(
-        projects.map((project) => {
-            return registry.createProject(project.metaPtr);
-        })
-    );
+    for (const project of projects) {
+        let tx = await registry.createProject(project.metaPtr);
+        console.log(`Created project in tx ${tx.hash}`);
+    }
 
     /* Create a program */
     const params = [
@@ -185,6 +235,7 @@ async function deployEverything() {
             programAddress = event.args.programContractAddress;
         }
     }
+    console.log(`Created a Program at ${programAddress}`);
 
     /* Create a round */
     const encodedParameters = generateAndEncodeRoundParam(
@@ -193,12 +244,15 @@ async function deployEverything() {
         account.address
     );
 
+    await getCurrentBlock();
+
     const roundTx = await roundFactoryContract.create(
         encodedParameters,
         programAddress // _ownedBy (Program)
     );
 
     const receipt = await roundTx.wait();
+    await getCurrentBlock();
     /* Clock starts ticking here */
     let roundAddress;
 
@@ -210,29 +264,38 @@ async function deployEverything() {
             roundAddress = event.args.roundAddress;
         }
     }
+    console.log(`Created a Round at ${roundAddress}`);
 
     const round = await ethers.getContractAt("RoundImplementation", roundAddress);
 
-    // /* Apply to round */
-    // await Promise.all(
-    //     projects.map((project) => {
-    //         return round.applyToRound(
-    //             ethers.utils.hexZeroPad(project.project, 32),
-    //             project.metaPtr
-    //         );
-    //     })
-    // );
-    //
-    // /* Approve 2 applications, reject last */
-    // await Promise.all(
-    //     projects.slice(0, 2).map((project) => {
-    //         return round.setApplicationStatuses(
-    //
-    //         );
-    //     })
-    // );
+    /* Apply to round */
+    for (const project of projects) {
+        await getCurrentBlock();
+        await round.applyToRound(
+            ethers.utils.hexZeroPad(project.project, 32),
+            project.metaPtr
+        );
+    }
+
+    const indexes: number[] = [0, 1, 2, 3];
+    const statusArray: number[] = [
+        STATUS.PENDING,
+        STATUS.ACCEPTED,
+        STATUS.REJECTED,
+        STATUS.CANCELED,
+    ];
+
+    const newState = buildNewState(BigInt(0), indexes, statusArray);
+
+    const applicationStatus = {
+        index: 0,
+        statusRow: newState,
+    }
+
+    await round.setApplicationStatuses([applicationStatus]);
 
     /* Wait until round starts */
+
 
     /* Cast 3-4 votes for each project */
 }
@@ -274,9 +337,9 @@ const generateAndEncodeRoundParam = async (
     /* All timestamps are in seconds*/
     const initRoundTime = [
         _currentTimestamp, // immediate  appStartTime
-        _currentTimestamp + SECONDS_PER_SLOT * 2, // 2 slots, roughly 30 secodns on goerli  appEndTime
-        _currentTimestamp + SECONDS_PER_SLOT * 2 + 1, // immediately after applications end  roundStartTime
-        _currentTimestamp + SECONDS_PER_SLOT * 5, // 75 seconds   roundEndTime
+        _currentTimestamp + SECONDS_PER_SLOT * 4, // 2 slots, roughly 30 seconds on goerli  appEndTime
+        _currentTimestamp + SECONDS_PER_SLOT * 5, // immediately after applications end  roundStartTime
+        _currentTimestamp + SECONDS_PER_SLOT * 8, // 75 seconds   roundEndTime
     ];
 
     const initMetaPtr = [roundMetaPtr, applicationMetaPtr];
@@ -297,6 +360,7 @@ const generateAndEncodeRoundParam = async (
         initRoles,
     ];
 
+    console.log(params);
     return encodeRoundParameters(params);
 };
 
