e9b4826a79819cb4557e8766a620d8029fc6bf86
bhargavaparoksham <aparokshambhargav@gmail.com>
2023-05-09 15:30:53 +0530

    feat: upgrade goerli contracts

--- scripts/config/payoutStrategy.config.ts
+++ scripts/config/payoutStrategy.config.ts
@@ -15,7 +15,7 @@ export const MerklePayoutParams: DeployParams = {
   },
   goerli: {
     factory: "0xE2Bf906f7d10F059cE65769F53fe50D8E0cC7cBe",
-    implementation: "0x3e7f72DFeDF6ba1BcBFE77A94a752C529Bb4429E",
+    implementation: "0xC808c9Ea4020E0F6Ec20715EEA0642fA6870B5Cc",
     contract: "0x3172a6cCE26529e7DD2B533e7c3622a0b544f349",
   },
   "optimism-mainnet": {
